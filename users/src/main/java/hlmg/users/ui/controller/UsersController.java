0b3de77aa421d3eb4114ca72c182299041a93b60
hlmg <devhlmg@gmail.com>
2024-08-30 14:37:30 +0900

    랜덤한 instance id를 설정하여 다중 인스턴스가 등록될 수 있도록 함

--- users/src/main/java/hlmg/users/ui/controller/UsersController.java
+++ users/src/main/java/hlmg/users/ui/controller/UsersController.java
@@ -1,15 +1,19 @@
 package hlmg.users.ui.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
 public class UsersController {
+    private final Environment env;
 
     @GetMapping("/status/check")
     public String status() {
-        return "Working";
+        return "Working on port " + env.getProperty("local.server.port");
     }
 }
