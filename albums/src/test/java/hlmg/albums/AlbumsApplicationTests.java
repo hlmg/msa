0b58d500e8ba448f003e1cc70722697807db87eb
hlmg <devhlmg@gmail.com>
2024-09-15 04:51:20 +0900

    import albums module

---
+++ albums/src/test/java/hlmg/albums/AlbumsApplicationTests.java
@@ -0,0 +1,14 @@
+package hlmg.albums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AlbumsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
