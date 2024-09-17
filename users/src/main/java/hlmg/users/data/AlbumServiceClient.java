3fd46214b0dcb02de7bafe13635ef254aadac625
hlmg <devhlmg@gmail.com>
2024-09-17 15:13:47 +0900

    resilience4j: retry

--- users/src/main/java/hlmg/users/data/AlbumServiceClient.java
+++ users/src/main/java/hlmg/users/data/AlbumServiceClient.java
@@ -2,6 +2,7 @@ package hlmg.users.data;
 
 import hlmg.users.ui.model.AlbumResponse;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import java.util.List;
 @FeignClient(name = "albums-ws")
 public interface AlbumServiceClient {
 
+    @Retry(name = "albums-ws")
     @CircuitBreaker(name = "albums-ws", fallbackMethod = "getAlbumsFallback")
     @GetMapping("/users/{id}/albums")
     List<AlbumResponse> getAlbums(@PathVariable String id);
