da34a480e0436f46c8c015af3b5548925ad650d2
hlmg <devhlmg@gmail.com>
2024-09-17 14:36:42 +0900

    resilience4j: circuit breaker

--- users/src/main/java/hlmg/users/data/AlbumServiceClient.java
+++ users/src/main/java/hlmg/users/data/AlbumServiceClient.java
@@ -1,15 +1,24 @@
 package hlmg.users.data;
 
 import hlmg.users.ui.model.AlbumResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Collections;
 import java.util.List;
 
 @FeignClient(name = "albums-ws")
 public interface AlbumServiceClient {
 
+    @CircuitBreaker(name = "albums-ws", fallbackMethod = "getAlbumsFallback")
     @GetMapping("/users/{id}/albums")
     List<AlbumResponse> getAlbums(@PathVariable String id);
+
+    default List<AlbumResponse> getAlbumsFallback(String id, Throwable exception) {
+        System.out.println("Param =  " + id);
+        System.out.println("Exception took place: " + exception.getMessage());
+        return Collections.emptyList();
+    }
 }
